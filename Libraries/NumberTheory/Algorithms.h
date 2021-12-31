40f110ad7857556ee71aaf45df971221f8977288
Daniel Pustotin <heartsker@gmail.com>
2021-12-31 14:46:55 +0300

    Number Theory small improvements and bug-research

--- Libraries/NumberTheory/Algorithms.h
+++ Libraries/NumberTheory/Algorithms.h
@@ -9,21 +9,22 @@
 
 namespace nt {
 
+    // CHECK: (DS) - Why do we need this?
     using std::gcd;
     using std::lcm;
 
     template<class M, class N, class Type = std::common_type_t<M, N>>
     constexpr Type binpow(M a, N n) {
-        static_assert(std::is_integral_v<M>, "binpow arguments are integers");
-        static_assert(!std::is_same_v<std::remove_cv_t<M>, bool>, "binpow arguments are not bools");
+        static_assert(std::is_integral_v<M>, "binpow base argument must be an integer");
+        static_assert(!std::is_same_v<std::remove_cv_t<M>, bool>, "binpow base argument must not be a bool");
 
-        static_assert(std::is_integral_v<N>, "binpow arguments are integers");
-        static_assert(!std::is_same_v<std::remove_cv_t<N>, bool>, "binpow arguments are not bools");
+        static_assert(std::is_integral_v<N>, "binpow exponent argument must be an integer");
+        static_assert(!std::is_same_v<std::remove_cv_t<N>, bool>, "binpow exponent argument must not be a bool");
 
         Type result = 1;
 
         while (n) {
-            if (n % 2) {
+            if (n & 1) {
                 result *= a;
                 --n;
             } else {
@@ -37,9 +38,10 @@ namespace nt {
 
     template<class M, class N, class K>
     constexpr std::common_type_t<M, N, K> binpow(M a, N n, K mod) {
-        static_assert(std::is_integral_v<K>, "binpow arguments are integers");
-        static_assert(!std::is_same_v<std::remove_cv_t<K>, bool>, "binpow arguments are not bools");
+        static_assert(std::is_integral_v<K>, "binpow modulo argument must be an integer");
+        static_assert(!std::is_same_v<std::remove_cv_t<K>, bool>, "binpow modulo argument must not be a bool");
 
+        // CHECK: (DS) - Is it true, that (a ^ n) mod m equals to (a mod m) ^ (n mod m)
         Modular modular_a(a, mod), modular_n(n, mod);
         return binpow(modular_a, modular_n);
     }
--- Libraries/NumberTheory/Modular.h
+++ Libraries/NumberTheory/Modular.h
@@ -9,33 +9,35 @@ namespace nt {
     template<class T>
     class Modular {
     public:
-        static_assert(std::is_integral_v<T>, "Modular mod is integer");
-        static_assert(!std::is_same_v<std::remove_cv_t<T>, bool>, "Modular mod is not bool");
+        static_assert(std::is_integral_v<T>, "Modular number must be an integer");
+        static_assert(!std::is_same_v<std::remove_cv_t<T>, bool>, "Modular number must not be a bool");
 
-        constexpr Modular(T value, T mod) : m_value(value % mod), m_mod(mod) {}
+        // FIX_ME: - % does not work fine for all test cases we want (e.g. negative values)
+        // Can we make % operator faster?
+        constexpr Modular(T t_value, T t_modulo) : value(t_value % t_modulo), modulo(t_modulo) {}
 
         template<class U>
         Modular &operator=(const U &other) {
             if constexpr(std::is_same_v<std::remove_cv_t<U>, Modular>) {
                 if (this != &other) {
-                    m_value = other.m_value;
-                    m_mod = other.m_mod;
+                    value = other.value;
+                    modulo = other.modulo;
                 }
             } else {
-                m_value = other;
-                m_value %= m_mod;
+                value = other;
+                value %= modulo;
             }
 
             return *this;
         }
 
         friend std::ostream &operator<<(std::ostream &out, const Modular m) {
-            out << m.m_value;
+            out << m.value;
             return out;
         }
 
     private:
-        T m_value, m_mod;
+        T value, modulo;
     };
 
 }
